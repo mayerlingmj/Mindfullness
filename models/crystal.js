9b16849fa1e417cc561b2b457500eefd484188dc
Mayerling Joseph <josephmayerling@gmail.com>
2023-05-30 09:46:52 -0400

    mindfulness

---
+++ models/crystal.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+//need to have many different crystals belonging to many users
+
+const crystalSchema = new mongoose.Schema({
+  name: String,
+  color: String,
+  description: String,
+  image: String,
+  userCreated: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  usersAddedToCollection: [
+    { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+  ]
+})
+
+module.exports = mongoose.model('Crystal', crystalSchema)
