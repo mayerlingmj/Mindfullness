9954b45064d5b4120f70e6a5d285736ba273dc99
Mayerling Joseph <josephmayerling@gmail.com>
2023-05-31 09:23:32 -0400

    mindfulness

--- mindfullness/routes/users.js
+++ mindfullness/routes/users.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
+var usersCtrl = require('../controllers/users')
+
+router.get('/login', usersCtrl.index)
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', function (req, res, next) {
+  res.send('respond with a resource')
+})
 
-module.exports = router;
+module.exports = router
