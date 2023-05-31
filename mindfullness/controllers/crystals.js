e4593b074bc0a447b0ce8bac0dd8019b5efe645c
Mayerling Joseph <josephmayerling@gmail.com>
2023-05-31 11:14:30 -0400

    mindfulness

--- mindfullness/controllers/crystals.js
+++ mindfullness/controllers/crystals.js
@@ -9,3 +9,11 @@ module.exports = {
   show,
   addToCollection
 }
+function addToCollection(req, res) {
+  Collection.findById(req.body.collectionId, function (err, collection) {
+    collection.crystalsAdded.push(req.params.id)
+    collection.save(function (e) {
+      res.redirect(`/crystals/${req.params.id}`)
+    })
+  })
+}
