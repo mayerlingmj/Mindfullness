80c65f28bdeaf1471d2393697675637c3560b281
Mayerling Joseph <josephmayerling@gmail.com>
2023-05-31 09:59:05 -0400

    mindfulness

--- mindfullness/controllers/collections.js
+++ mindfullness/controllers/collections.js
@@ -11,3 +11,17 @@ module.exports = {
   delete: deleteCollection,
   removeFromCollection
 }
+
+async function removeFromCollection(req, res) {
+  try {
+    // find the collection we're removing a chakra and crystal from
+    const collectionDoc = await Collection.findById(req.params.collectionId)
+    // remove the chakra and crystal by the id
+    collectionDoc.chakrasAdded.remove(req.params.chakraId)
+    collectionDoc.save(function (err) {
+      res.redirect(`/collections/${collectionDoc._id}/edit`)
+    })
+  } catch (err) {
+    res.send(err)
+  }
+}
