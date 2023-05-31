c29a9ba057c221dc45efed618e482ab7154ffe51
Mayerling Joseph <josephmayerling@gmail.com>
2023-05-31 09:55:04 -0400

    mindfulness

--- mindfullness/controllers/collections.js
+++ mindfullness/controllers/collections.js
@@ -1,3 +1,13 @@
 const Chakra = require('../models/chakra')
 const Crystal = require('../models/crystal')
 const Collection = require('../models/collection')
+
+module.exports = {
+  create,
+  index,
+  show,
+  edit,
+  update,
+  delete: deleteCollection,
+  removeFromCollection
+}
