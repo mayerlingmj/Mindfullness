35377230b73ab2d110608cdedb9b2918c70fe330
Mayerling Joseph <josephmayerling@gmail.com>
2023-05-31 11:30:15 -0400

    mindfulness

--- mindfullness/models/collection.js
+++ mindfullness/models/collection.js
@@ -4,7 +4,8 @@ const collectionSchema = new mongoose.Schema({
   name: String,
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   userName: String,
-  chakrasAdded: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Chakra' }]
+  chakrasAdded: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Chakra' }],
+  crystalsAdded: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Crystal' }]
 })
 
 module.exports = mongoose.model('Collection', collectionSchema)
